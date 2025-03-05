511813748f83e2e724c856bcbf8cfe70e5e21bd5
gaqlaxy <v4vripper@gmail.com>
2025-03-05 07:09:58 +0530

    2

--- src/Components/LatestProducts.jsx
+++ src/Components/LatestProducts.jsx
@@ -1,22 +1,54 @@
-import React, {useState} from 'react'
-import LatestProds from '../data/latestprods.json'
+import React, { useState } from "react";
+import LatestProds from "../data/latestprods.json";
 console.log(LatestProds);
 
 export default function LatestProducts() {
-    
-
   return (
     <>
-    <div>
-        <div className='flex justify-between items-center mx-12 my-5'>
-            <h1 className='text-4xl text-black font-bold'>Our Best Products</h1>
-            <p className='text-sm'>We have solutions to help you achieve smoother, <br />cleaner and more youthful looking skin.</p>
+      <div>
+        <div className="flex justify-between items-center mx-12 my-5">
+          <h1 className="text-4xl text-black font-bold">Our Best Products</h1>
+          <p className="text-sm">
+            We have solutions to help you achieve smoother, <br />
+            cleaner and more youthful looking skin.
+          </p>
         </div>
-        
+        <div>
+          <div className="grid grid-cols-4 gap-4 mx-12 my-12 relative">
+            <div className="card relative">
+              <img
+                src="https://images.unsplash.com/photo-1609175214983-594001465d18?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className=" h-[18.75rem] w-[18.75rem] rounded-2xl"
+              />
 
-
-    </div>
-    {/* <div className='mx-12 my-5 flex items-center justify-around relative'>
+              <span class="material-symbols-outlined">arrow_outward</span>
+            </div>
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1609175214983-594001465d18?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className=" h-[18.75rem] w-[18.75rem] rounded-2xl"
+              />
+            </div>
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1609175214983-594001465d18?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className=" h-[18.75rem] w-[18.75rem] rounded-2xl"
+              />
+            </div>
+            <div className="card">
+              <img
+                src="https://images.unsplash.com/photo-1609175214983-594001465d18?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className=" h-[18.75rem] w-[18.75rem] rounded-2xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className='mx-12 my-5 flex items-center justify-around relative'>
         <div>
             <h1 className='text-4xl text-black font-bold'>Our Best <br /> <span className='text-[#0e402d]'>Products</span></h1>
             <button>Left</button>
@@ -49,10 +81,11 @@ export default function LatestProducts() {
         </div>
     </div> */}
     </>
-  )
+  );
 }
 
-{/* <div className='grid grid-cols-3 gap-4'>
+{
+  /* <div className='grid grid-cols-3 gap-4'>
     {LatestProds.latprod.map((prod) => {
         return (
             <div>
@@ -63,4 +96,5 @@ export default function LatestProducts() {
             </div>
         )
     })}
-</div> */}
+</div> */
+}
