8296e34190cf9b7e7da26081bc0e1c1e488c2164
gaqlaxy <v4vripper@gmail.com>
2025-03-06 06:20:23 +0530

    Some tweaks

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeroSection() {
   return (
     <>
-      <div className="h-full">
+      <div className="h-full w-full">
         <svg className="svg-defs" aria-hidden="true">
           <defs>
             <clipPath id="shapeVelyVely" clipPathUnits="userSpaceOnUse">
